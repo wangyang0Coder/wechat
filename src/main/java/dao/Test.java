902ec0006cff25a61a509b92c860441c4deef1b5
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-05-01 11:38:34 +0800

    Initial commit

---
+++ src/main/java/dao/Test.java
@@ -0,0 +1,9 @@
+package dao;
+
+/**
+ * @Author: Azhu
+ * @Date: 2019/5/1 11:34
+ * Description:
+ */
+public class Test {
+}
---
+++ src/main/java/service/Test.java
@@ -0,0 +1,9 @@
+package service;
+
+/**
+ * @Author: Azhu
+ * @Date: 2019/5/1 11:35
+ * Description:
+ */
+public class Test {
+}
