6a9d196fa0cd525d4914d51a487e6c4c4b6f270d
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-05-06 10:47:32 +0800

    照抄了别人的Util包其中对Jsion、文件和消息类型的枚举类的处理

---
+++ src/main/java/util/ChatType.java
@@ -0,0 +1,17 @@
+package util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author: Azhu
+ * @Date: 2019/5/6 10:03
+ * Description:描述消息类型的枚举类
+ */
+public enum ChatType {
+    REGISTER,SINGLE_SENDING,GROUP_SENDING,FILE_MSG_SINGLE_SENDING,FILE_MSG_GROUP_SENDING;
+    private static final Logger logger= LoggerFactory.getLogger(ChatType.class);
+    public static void main(String[] args) {
+        logger.info(String.valueOf(ChatType.REGISTER));
+    }
+}
---
+++ src/main/java/util/Constant.java
@@ -0,0 +1,40 @@
+package util;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
+import model.po.GroupInfo;
+import model.po.UserInfo;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @Author: Azhu
+ * @Date: 2019/5/6 10:07
+ * Description:
+ * * 描述: 全局常量
+ *      1. USER_TOKEN 用户认证的键，用来匹配http session中的对应userId；
+ *      2. webSocketServerHandshaker表，用channelId为键，存放握手实例。用来响应CloseWebSocketFrame的请求；
+ *      3. onlineUser表，用userId为键，存放在线的客户端连接上下文；
+ *      4. groupInfo表，用groupId为键，存放群信息；
+ *      5. userInfo表，用username为键，存放用户信息。
+ */
+public class Constant {
+    public static final String USER_TOKEN="userId";
+
+    public static Map<String,WebSocketServerHandshaker>  webSocketServerHandshaker=
+            new ConcurrentHashMap<String, WebSocketServerHandshaker>();
+
+    public static Map<String,ChannelHandlerContext> onlineUserMap=
+            new ConcurrentHashMap<String,ChannelHandlerContext>();
+
+    public static Map<String,GroupInfo> groupInfoMap=
+            new ConcurrentHashMap<String,GroupInfo>();
+
+    public static Map<String,UserInfo> userInfoMap=
+            new HashMap<String,UserInfo>();
+}
